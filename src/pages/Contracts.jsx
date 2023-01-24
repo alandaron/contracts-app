4327df8d974fb6e55c8c66cd4101a3256e05ded5
Aron <aronaland4@gmail.com>
2023-01-24 22:30:42 +0200

    faili üles laadimine

--- src/pages/Contracts.jsx
+++ src/pages/Contracts.jsx
@@ -1,13 +1,20 @@
+import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
-import { AiOutlineFileSearch } from "react-icons/ai";
-import { FaFileDownload } from "react-icons/fa";
+import { AiOutlineFileSearch, AiOutlineLoading } from "react-icons/ai";
+import {
+	FaFileDownload,
+	FaFileUpload,
+	FaRegCheckCircle,
+	FaRegTimesCircle,
+} from "react-icons/fa";
 import { IoChevronBackCircleOutline } from "react-icons/io5";
-
 import { Link, useParams } from "react-router-dom";
 
 function Contracts() {
 	const [contracts, setContracts] = useState([]);
+	const [selectedFile, setSelectedFile] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [isUploading, setIsUploading] = useState([]);
 	const { personal_code } = useParams();
 
 	useEffect(() => {
@@ -28,18 +35,72 @@ function Contracts() {
 					method: "GET",
 					headers: {
 						"Content-Type": "application/json",
-						// 'Content-Type': 'application/x-www-form-urlencoded',
 					},
 				}
 			);
 
 			const json = await response.json();
-			setContracts(json.data);
+			const sortedById = json.data.sort((a, b) => a.id - b.id);
+			setContracts(sortedById);
+			setSelectedFile(
+				sortedById.map((contract) => {
+					return { id: contract.id, file: undefined };
+				})
+			);
+			setIsUploading(
+				sortedById.map((contract) => {
+					return { id: contract.id, uploading: false };
+				})
+			);
 			setLoading(false);
 		} catch (error) {
 			console.error(error);
 			setLoading(false);
 			return;
+		}
+	};
+
+	const fileSelected = (contract_id) => {
+		const index = selectedFile.findIndex((e) => e.id === contract_id);
+		if (selectedFile[index].file === undefined) return false;
+		return true;
+	};
+
+	const fileUploading = (contract_id) => {
+		const index = isUploading.findIndex((e) => e.id === contract_id);
+		return isUploading[index].uploading;
+	};
+
+	const changeSelectedFile = (contract_id, file) => {
+		const index = selectedFile.findIndex((e) => e.id === contract_id);
+		selectedFile[index].file = file;
+		setSelectedFile([...selectedFile]);
+	};
+
+	const uploadFile = async (contract_id) => {
+		const { file } = selectedFile.find((e) => e.id === contract_id);
+		const index = isUploading.findIndex((e) => e.id === contract_id);
+		isUploading[index].uploading = true;
+		setIsUploading([...isUploading]);
+
+		try {
+			const form = new FormData();
+			form.append(`files`, file, file.name);
+			form.append(`ref`, "api::contract.contract");
+			form.append(`refId`, contract_id);
+			form.append(`field`, "contract_file");
+
+			await fetch(`https://strapi.aland.ee/api/upload`, {
+				method: "POST",
+				body: form,
+			});
+
+			getContracts();
+		} catch (error) {
+			console.error(error);
+		} finally {
+			isUploading[index].uploading = false;
+			setIsUploading([...isUploading]);
 		}
 	};
 
@@ -60,10 +121,8 @@ function Contracts() {
 			{loading ? (
 				<div className="text-black bg-white w-4/5 mx-auto py-3">
 					<div className="flex items-center justify-center">
-						<div className="flex items-center justify-center mr-3">
-							<div className="w-8 h-8 border-b-2 border-black rounded-full animate-spin"></div>
-						</div>
-						Palun oota...
+						<AiOutlineLoading className="animate-spin" size={26} />
+						<span className="ml-1">Palun oota...</span>
 					</div>
 				</div>
 			) : (
@@ -71,58 +130,31 @@ function Contracts() {
 					<table className="w-full">
 						<thead className="bg-white border-b">
 							<tr>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									#
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Isikukood
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Eesnimi
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Perenimi
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Töölepingu algus
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Töölepingu lõpp
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Tööleping aktiivne
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Fail
 								</th>
-								<th
-									scope="col"
-									className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
-								>
+								<th className="text-sm font-medium text-gray-900 px-4 py-4 text-left">
 									Laadi fail üles
 								</th>
 							</tr>
@@ -139,7 +171,10 @@ function Contracts() {
 								</tr>
 							)}
 							{contracts.map((contract) => (
-								<tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+								<tr
+									key={contract.id}
+									className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+								>
 									<td className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
 										{contract.id}
 									</td>
@@ -153,47 +188,84 @@ function Contracts() {
 										{contract.attributes.last_name}
 									</td>
 									<td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
-										{contract.attributes.start_date}
+										{format(
+											new Date(contract.attributes.start_date),
+											"dd.MM.yyyy"
+										)}
 									</td>
 									<td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
 										{contract.attributes.end_date
-											? contract.attributes.end_date
+											? format(
+													new Date(contract.attributes.end_date),
+													"dd.MM.yyyy"
+											  )
 											: "Tulevikus"}
 									</td>
 									<td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
 										{contract.attributes.active ? (
-											<span className="rounded-full bg-green-600 text-white px-5 py-1">
-												Jah
+											<span className="flex w-20 items-center gap-2 rounded-full bg-green-600 text-white px-5 py-1">
+												<FaRegCheckCircle />
+												<span>Jah</span>
 											</span>
 										) : (
-											<span className="rounded-full bg-red-600 text-white px-5 py-1">
-												Ei
+											<span className="flex w-20 items-center gap-2 rounded-full bg-red-600 text-white px-5 py-1">
+												<FaRegTimesCircle />
+												<span>Ei</span>
 											</span>
 										)}
 									</td>
 									<td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap flex gap-2">
 										{contract.attributes.contract_file.data && (
-											<a
-												href={`http://strapi.aland.ee${contract.attributes.contract_file.data.attributes.url}`}
-												target="_blank"
-												download
-											>
+											<>
+												<a
+													href={`http://strapi.aland.ee${contract.attributes.contract_file.data.attributes.url}`}
+													target="_blank"
+													download
+												>
+													<button className="flex items-center border border-black rounded-md p-1 bg-gray-800 text-white hover:bg-black">
+														<FaFileDownload />
+														<span className="ml-1">Laadi alla</span>
+													</button>
+												</a>
 												<button className="flex items-center border border-black rounded-md p-1 bg-gray-800 text-white hover:bg-black">
-													<FaFileDownload />
-													<span className="ml-1">Laadi alla</span>
+													<AiOutlineFileSearch />
+													<span className="ml-1">Vaata lepingut</span>
 												</button>
-											</a>
+											</>
 										)}
-										<button className="flex items-center border border-black rounded-md p-1 bg-gray-800 text-white hover:bg-black">
-											<AiOutlineFileSearch />
-											<span className="ml-1">Vaata lepingut</span>
-										</button>
 									</td>
 									<td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
-										<input
-											className="form-control block py-1 text-sm font-normal text-gray-700 bg-clip-padding rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											type="file"
-										/>
+										<div className="flex justify-between w-3/4">
+											<input
+												className="form-control w-full block py-1 text-sm font-normal text-gray-700 bg-clip-padding rounded transition ease-in-out m-0 focus:text-gray-700  focus:border-blue-600 focus:outline-none select-none"
+												type="file"
+												onChange={(e) =>
+													changeSelectedFile(contract.id, e.target.files[0])
+												}
+												disabled={fileUploading(contract.id)}
+											/>
+
+											<button
+												onClick={() => uploadFile(contract.id)}
+												className="flex items-center mx-6 border border-black rounded-md p-1 bg-gray-800 text-white hover:bg-black disabled:bg-gray-500 disabled:hover:none disabled:border-none disabled:text-gray-400"
+												disabled={
+													!fileSelected(contract.id) ||
+													fileUploading(contract.id)
+												}
+											>
+												{fileUploading(contract.id) ? (
+													<>
+														<AiOutlineLoading className="animate-spin" />
+														<span className="ml-1">Üles laadimine...</span>
+													</>
+												) : (
+													<>
+														<FaFileUpload />
+														<span className="ml-1">Laadi üles</span>
+													</>
+												)}
+											</button>
+										</div>
 									</td>
 								</tr>
 							))}
